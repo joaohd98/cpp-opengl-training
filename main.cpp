8a491a31f46d8989ea53b92ab66db8eeea6f02b0
João Damazio <joao.damazio@fiap.com.br>
2024-09-07 02:12:16 -0300

    Add glad

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
+#include <glad/glad.h>   // Include GLAD to load OpenGL functions
+// #include <GLFW/glfw3.h>
+
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main()
+{
+    std::cout << "Teste Glad!" << std::endl;
     return 0;
 }
